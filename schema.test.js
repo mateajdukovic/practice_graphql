b52f3de12f2695ce5b45ca209c6b50a7b9803250
mate@carbmanager.com <mate@carbmanager.com>
2023-04-05 14:55:20 +0200

    Added a test for the resolver using mock method from easygraphql-tester

--- schema.test.js
+++ schema.test.js
@@ -9,6 +9,7 @@ describe("Test GraphQL Schema with Jest", () => {
   });
 
   it("Should return a list of books", () => {
+    // Prepare query
     const query = `
       {
         books {
@@ -29,6 +30,7 @@ describe("Test GraphQL Schema with Jest", () => {
   });
 
   it("Should return a single author", () => {
+    // Prepare query
     const query = `
       {
         author(id: 1) {
@@ -49,6 +51,7 @@ describe("Test GraphQL Schema with Jest", () => {
   });
 
   it("Should return an error for an invalid query", () => {
+    // Prepare query
     const query = `
       {
         books {
@@ -61,5 +64,41 @@ describe("Test GraphQL Schema with Jest", () => {
     const result = tester.graphql(query);
     // expects errors to be returned in the result as there is no `nonExistentField` in the schema
     expect(result.errors).toBeDefined();
+  });
+});
+
+describe("Test GraphQL Resolver with Jest", () => {
+  it("Should fetch author by id", () => {
+    // Prepare query
+    const query = `
+      query($id: Int!) {
+        author(id: $id) {
+          id
+          name
+          books {
+            id
+            name
+          }
+        }
+      }
+    `;
+
+    const variables = { id: 1 };
+    // method to generate a mock result for the query
+    const result = tester.mock({ query, variables });
+    const expectedAuthor = authors.find((author) => author.id === variables.id);
+    // check if the result from the mocked query matches the expected values
+    expect(result.data.author).toEqual({
+      id: expectedAuthor.id.toString(),
+      name: expectedAuthor.name,
+      books: books
+        .filter((book) => book.authorId === expectedAuthor.id)
+        .map((book) => {
+          return {
+            id: book.id.toString(),
+            name: book.name,
+          };
+        }),
+    });
   });
 });
